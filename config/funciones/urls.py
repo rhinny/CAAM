f1e42acb2b52efaf2be73e51961a5a64ec827e26
ayleenenen <ayleenhuang@gmail.com>
2025-10-19 00:44:24 -0300

    calendario pero no se guardan las fechas en la base de datos aún

--- config/funciones/urls.py
+++ config/funciones/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path('calendario/', views.calendario, name="calendario"),
+               path('guardar_fechas/', views.guardar_fechas, name="guardar_fechas"),
+               path('borrar_fechas/', views.borrar_fechas, name="borrar_fecha")
                ]
--- config/funciones/views.py
+++ config/funciones/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from datetime import date, timedelta
+from django.http import JsonResponse
+from datetime import date, timedelta, datetime
 from .models import FechasDisponibles
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 def calendario(request):
     # Obtener mes actual
-    hoy = date.today() #fecha actual
+    hoy = date.today() #fecha actual YYYY-MM-DD
     primer_dia = hoy.replace(day=1) #primer día del mes actual
     ultimo_dia = (primer_dia.replace(month=hoy.month % 12 + 1, day=1) - timedelta(days=1)) #último día del mes actual
     
@@ -23,3 +25,23 @@ def calendario(request):
         calendario.append({"fecha": dia, "eventos": eventos_dia})
 
     return render(request, "calendario.html", {"calendario": calendario, "mes": hoy.strftime("%B")})
+
+@csrf_exempt
+def guardar_fechas(request):
+    if request.method == "POST":
+        fecha = request.POST.get('fecha')
+        if fecha:
+            fecha_obj = datetime.strptime(fecha, '%Y-%m-%d').date()
+            FechasDisponibles.objects.get_or_create(estudiante=request.user, fecha=fecha_obj)
+            return JsonResponse({'status': 'ok'})
+    return JsonResponse({'status': 'error'})
+
+@csrf_exempt
+def borrar_fechas(request):
+    if request.method == "POST":
+        fecha = request.POST.get('fecha')
+        if fecha:
+            fecha_obj = datetime.strptime(fecha, '%Y-%m-%d').date()
+            FechasDisponibles.objects.filter(estudiante=request.user, fecha=fecha_obj).delete()
+            return JsonResponse({'status': 'ok'})
+    return JsonResponse({'status': 'error'})
