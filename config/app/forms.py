b71fce61db113398cbd60d8e0ddf4ae22712fbfc
ayleenenen <ayleenhuang@gmail.com>
2025-11-16 16:26:00 -0300

    registro en español sin mensajes de validaciones

--- config/app/forms.py
+++ config/app/forms.py
@@ -6,6 +6,11 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = Usuarios
         fields = ['tipo','rut','email','username', 'first_name', 'last_name', 'password1','password2']
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].help_text = ""
+        self.fields["password1"].help_text = ""
+        self.fields["password2"].help_text = ""
 
 class ComunaForm(forms.ModelForm):
     class Meta:
